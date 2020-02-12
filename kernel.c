ee05fcb203780b5d063db93784d20cd23abd10c7
Fatkhan Masruri <52845259+fatkhanm@users.noreply.github.com>
2020-02-12 20:37:24 +0700

    readstring printstring

---
+++ kernel.c
@@ -0,0 +1,58 @@
+void printString(char *string);
+void readString(char *string);
+void readSector(char *buffer, int sector);
+int mod(int angka1,int angka2);
+int div(int angak1, int angka2);
+int main () {
+  char* hu;
+  putInMemory(0xB000, 0x8000, 'H');
+  putInMemory(0xB000, 0x8001, 0xD);
+  putInMemory(0xB000, 0x8002, 'a');
+  putInMemory(0xB000, 0x8003, 0xD);
+  putInMemory(0xB000, 0x8004, 'i');
+  putInMemory(0xB000, 0x8005, 0xD);
+  printString("1 2 3 4 5");
+  readString(hu);
+  printString(hu);
+  while (1);
+}
+
+void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
+void printString(char* string){
+    int i;
+    i = 0;
+    while(string[i]!='\0'){
+        interrupt(0x10,0xE00+string[i],0,0,0);
+        i++;
+    }
+}
+void readString(char* string){
+    char in = interrupt(0x16,0,0,0,0);
+    int kursor = 0;
+    while(in != '\r'){
+        if(in == '\b'){
+            if(kursor>0){
+                interrupt(0x10,0xE00 + '\b',0,0,0);
+                interrupt(0x10,0xE00 + '\0',0,0,0);
+                interrupt(0x10,0xE00 + '\b',0,0,0);
+                kursor-=1;
+            }
+        }else{
+            string[kursor] = in;
+            interrupt(0x10,0xE00 + string[kursor],0,0,0);
+            kursor+=1;
+        }
+        in = interrupt(0x16,0,0,0,0);
+    }
+    string[kursor] = '\0';
+    interrupt(0x10,0xE00+'\n',0,0,0);
+    interrupt(0x10,0xe00+'\r',0,0,0);
+
+}
+int mod(int angka1, int angka2){
+    while(angka1>angka2){
+        angka1 -=angka2;
+    }
+    return anagka1;
+}
+
